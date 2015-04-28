4dad40f38dce7150f2ec7018137795a7f307a864
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2015-04-28 14:59:46 +0200

    Use new Spoofax Maven plugin build system and use consistent meta-language naming.

---
+++ org.metaborg.meta.lang.template/editor/java/org/metaborg/meta/lang/template/strategies/InteropRegisterer.java
@@ -0,0 +1,14 @@
+package org.metaborg.meta.lang.template.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+/**
+ * Helper class for {@link unquote_0_0}.
+ */
+public class InteropRegisterer extends JavaInteropRegisterer {
+
+  public InteropRegisterer() {
+    super(new Strategy[] { unquote_0_0.instance });
+  }
+}
