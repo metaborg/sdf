28e4c163b6cd55873404de9f5ff3c3ca8541b88e
Jasper Denkers <j.denkers@tudelft.nl>
2019-03-13 19:50:28 +0100

    Document IActionQuery

--- org.metaborg.tableinterfaces/src/main/java/org/metaborg/parsetable/IActionQuery.java
+++ org.metaborg.tableinterfaces/src/main/java/org/metaborg/parsetable/IActionQuery.java
@@ -2,8 +2,16 @@ package org.metaborg.parsetable;
 
 public interface IActionQuery {
 
+    /**
+     * The lookahead character to query actions for.
+     */
     int actionQueryCharacter();
 
+    /**
+     * Retrieve lookahead with a given length for the current action query. During a parse round, when a character is
+     * shifted, that character is the actual first character of lookahead (as returned by actionQueryCharacter). The
+     * lookahead returned by this function starts after that character.
+     */
     String actionQueryLookahead(int length);
 
 }
