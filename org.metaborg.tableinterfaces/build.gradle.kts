b563ebd325f10adebea5baa5c77d95524684a0aa
Gabriel Konat <gabrielkonat@gmail.com>
2019-05-07 15:02:36 +0200

    Add Gradle build files.

---
+++ org.metaborg.tableinterfaces/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+  id("org.metaborg.gradle.config.junit-testing")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "sdf"
+
+pluginManagement {
+  repositories {
+    // Get plugins from artifacts.metaborg.org, first.
+    maven("https://artifacts.metaborg.org/content/repositories/releases/")
+    maven("https://artifacts.metaborg.org/content/repositories/snapshots/")
+    // Required by several Gradle plugins (Maven central).
+    maven("https://artifacts.metaborg.org/content/repositories/central/") // Maven central mirror.
+    mavenCentral() // Maven central as backup.
+    // Get plugins from Gradle plugin portal.
+    gradlePluginPortal()
+  }
+}
+
+fun includeProject(id: String, path: String = "org.metaborg.$id") {
+  include(id)
+  project(":$id").projectDir = file(path)
+}
+
+includeProject("tableinterfaces")
+includeProject("characterclasses")
+includeProject("sdf2table")
