7a7c2126c5bc3858c5aacf8db26ce5578a66e8d3
Gabriel Konat <gabrielkonat@gmail.com>
2019-09-17 15:55:50 +0200

    Prevent creation of anonymous classes

--- org.metaborg.sdf2table/src/main/java/org/metaborg/sdf2table/grammar/layoutconstraints/LayoutConstraintComparisonOperator.java
+++ org.metaborg.sdf2table/src/main/java/org/metaborg/sdf2table/grammar/layoutconstraints/LayoutConstraintComparisonOperator.java
@@ -1,29 +1,19 @@
 package org.metaborg.sdf2table.grammar.layoutconstraints;
 
 public enum LayoutConstraintComparisonOperator {
-    GT {
-        @Override public String toString() {
-            return ">";
-        }
-    },
-    GE {
-        @Override public String toString() {
-            return ">=";
-        }
-    },
-    LT {
-        @Override public String toString() {
-            return "<";
-        }
-    },
-    LE {
-        @Override public String toString() {
-            return "<=";
-        }
-    },
-    EQ {
-        @Override public String toString() {
-            return "==";
-        }
+    GT(">"),
+    GE(">="),
+    LT("<"),
+    LE("<="),
+    EQ("==");
+
+    private String str;
+
+    LayoutConstraintComparisonOperator(String str) {
+        this.str = str;
+    }
+
+    @Override public String toString() {
+        return str;
     }
 }
