ce9422a1394820741a3128f84e012b28bf420c8d
Gabriel Konat <gabrielkonat@gmail.com>
2019-09-19 13:13:32 +0200

    Don't use parent POM in Gradle, as it causes a dependency cycle because it manages a dependency to PIE

--- org.metaborg.parsetable/build.gradle.kts
+++ org.metaborg.parsetable/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 dependencies {
-  api(platform("org.metaborg:parent:$version"))
+  // api(platform("org.metaborg:parent:$version")) // Can't use: causes dependency cycle because parent mentions pie.
 
   api("org.metaborg:org.spoofax.terms:$version")
-  compileOnly("com.google.code.findbugs:jsr305")
-  api("com.google.guava:guava")
-  testCompileOnly("junit:junit")
+  compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+  api("com.google.guava:guava:26.0-jre")
+  testCompileOnly("junit:junit:4.12")
   testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.1.0")
 }
