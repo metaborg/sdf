a45b27bbf170bda1c50f864e4a2d25e1b6ceacbd
Gabriel Konat <gabrielkonat@gmail.com>
2020-03-23 15:11:09 +0100

    Update Gradle build file.

--- org.metaborg.meta.lang.template/build.gradle.kts
+++ org.metaborg.meta.lang.template/build.gradle.kts
@@ -12,13 +12,6 @@ ecj {
   toolVersion = "3.20.0"
 }
 
-// HACK: Temporarily set group to 'org.metaborgg' to prevent substitution of baseline version of SDF3 to this project.
+// HACK: Temporarily set group to 'org.metaborggggg' to prevent substitution of baseline version of SDF3 to this project.
 // I could not find another way to disable this substitution.
-group = "org.metaborgg"
-// Set group back to 'org.metaborg' before build is executed.
-val fixGroupTask = tasks.register("fixGroup") {
-  group = "org.metaborg"
-}
-tasks.named("build") {
-  dependsOn(fixGroupTask)
-}
+group = "org.metaborggggg"
