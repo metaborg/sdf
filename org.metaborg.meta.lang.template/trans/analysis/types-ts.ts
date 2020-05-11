f33e4d254a1bd809feabb6538bdfe74b31dbdebd
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-05-11 11:06:53 +0200

    Whitespace cleanup

--- org.metaborg.meta.lang.template/trans/analysis/types-ts.ts
+++ org.metaborg.meta.lang.template/trans/analysis/types-ts.ts
@@ -1,32 +1,31 @@
 module analysis/types-ts
 
-imports 
+imports
   signatures/TemplateLang-sig
   libstrc
   analysis/types
 
 type rules
-	
-	
-	//When a sort has multiple definitions something like
+
+
+  //When a sort has multiple definitions something like
     //Iter(Sort(s)) : ListType(s) doesn't work properly
     //In this case, I just want the type for Iter(Sort("A")) to be ListType("A")
     //but A carry out its annotations which include the multiple definitions
-	
-	//Iter(Sort(s)) : ListType(s)
-	
-    
-    
+
+  //Iter(Sort(s)) : ListType(s)
+
+
+
     Placeholder(s, opt) : t
-    	where s : t
-    
-    
-    
-    
+      where s : t
+
+
+
+
    // Placeholder(_, s, None(), _) : t
     //	where s : t
-    	
-    
-    	
-    	
-    
+
+
+
+
