c6b9dab5fe6ed969a6469705d2597233c6312e5f
Jasper Denkers <j.denkers@tudelft.nl>
2020-11-25 15:45:57 +0100

    Move filtering of non-recovery actions up to IActionsForCharacter

--- org.metaborg.parsetable/src/main/java/org/metaborg/parsetable/query/IActionsForCharacter.java
+++ org.metaborg.parsetable/src/main/java/org/metaborg/parsetable/query/IActionsForCharacter.java
@@ -1,5 +1,10 @@
 package org.metaborg.parsetable.query;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.metaborg.parsetable.actions.ActionType;
 import org.metaborg.parsetable.actions.IAction;
 import org.metaborg.parsetable.actions.IReduce;
 
@@ -21,5 +26,28 @@ public interface IActionsForCharacter {
      * and lookahead) and parsing mode.
      */
     Iterable<IReduce> getApplicableReduceActions(IActionQuery actionQuery, ParsingMode parsingMode);
+
+    default ActionsPerCharacterClass[] filterNonRecoveryActions(ActionsPerCharacterClass[] actionsPerCharacterClasses) {
+        ActionsPerCharacterClass[] filteredActionsPerCharacterClasses =
+            new ActionsPerCharacterClass[actionsPerCharacterClasses.length];
+
+        int i = 0;
+        for(ActionsPerCharacterClass actionsPerCharacterClass : actionsPerCharacterClasses) {
+            List<IAction> filteredActions = new ArrayList<>(actionsPerCharacterClass.actions);
+
+            filteredActions.removeIf(action -> {
+                return (action.actionType() == ActionType.REDUCE || action.actionType() == ActionType.REDUCE_LOOKAHEAD)
+                    && ((IReduce) action).production().isRecovery();
+            });
+
+            if(filteredActions.size() == actionsPerCharacterClass.actions.size())
+                filteredActionsPerCharacterClasses[i++] = actionsPerCharacterClass;
+            else if(filteredActions.size() > 0)
+                filteredActionsPerCharacterClasses[i++] =
+                    new ActionsPerCharacterClass(actionsPerCharacterClass.characterClass, filteredActions);
+        }
+
+        return Arrays.copyOf(filteredActionsPerCharacterClasses, i);
+    }
 
 }
