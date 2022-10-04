29cc9b8f39949fd5ef8346e3cad5181126af81d4
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2022-10-04 18:10:02 +0200

    Remove deprecated ECJ build dependency

--- sdf3.ext.statix/build.gradle.kts
+++ sdf3.ext.statix/build.gradle.kts
@@ -3,7 +3,6 @@ import org.metaborg.core.language.*
 plugins {
   id("org.metaborg.gradle.config.java-library")
   id("org.metaborg.devenv.spoofax.gradle.langspec")
-  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
   `maven-publish`
 }
 
@@ -33,11 +32,4 @@ metaborg { // Do not create Java publication; this project is already published 
   javaCreatePublication = false
   javaCreateSourcesJar = false
   javaCreateJavadocJar = false
-}
-
-ecj {
-  toolVersion = "3.21.0"
-}
-tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
-  options.headerOutputDirectory.convention(provider { null })
 }
