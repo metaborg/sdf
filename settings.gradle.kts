b128dcbc20b55bcb884b61a31ab2c80a27d4b831
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-02-23 16:41:37 +0100

    Create root Gradle file

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "sdf-project"
+
+// This is needed to let Gradle find the dependencies of the spoofax2-gradle plugin
+pluginManagement {
+  repositories {
+    maven("https://artifacts.metaborg.org/content/groups/public/")
+  }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from("dev.spoofax:spoofax3-catalog:0.0.0-SNAPSHOT")
+        }
+    }
+}
+
+//include("org.metaborg.meta.lang.template")
+//include("org.metaborg.parsetable")
+//include("org.metaborg.sdf2parenthesize")
+//include("org.metaborg.sdf2table")
+//include("sdf3.ext.statix")
