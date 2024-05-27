2c8fbb1d1b5d4d27d80646872a299c7ad137a558
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:38:44 +0200

    Reformat code to match Kotlin official style

--- org.metaborg.sdf2parenthesize/build.gradle.kts
+++ org.metaborg.sdf2parenthesize/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  `java-test-fixtures`
+    id("org.metaborg.gradle.config.java-library")
+    `java-test-fixtures`
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  implementation(project(":sdf2table"))
-  api(project(":org.metaborg.parsetable"))
-  implementation(compositeBuild("org.strategoxt.strj"))
-  implementation(compositeBuild("org.metaborg.util"))
+    implementation(project(":sdf2table"))
+    api(project(":org.metaborg.parsetable"))
+    implementation(compositeBuild("org.strategoxt.strj"))
+    implementation(compositeBuild("org.metaborg.util"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 }
--- org.metaborg.sdf2table/build.gradle.kts
+++ org.metaborg.sdf2table/build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
-  `java-test-fixtures`
+    id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.junit-testing")
+    `java-test-fixtures`
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(compositeBuild("org.metaborg.util"))
+    api(compositeBuild("org.metaborg.util"))
 
-  api(project(":org.metaborg.parsetable"))
-  implementation("jakarta.annotation:jakarta.annotation-api")
-  testImplementation(testFixtures(project(":org.metaborg.parsetable")))
-  testCompileOnly("junit:junit")
-  testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    api(project(":org.metaborg.parsetable"))
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    testImplementation(testFixtures(project(":org.metaborg.parsetable")))
+    testCompileOnly("junit:junit")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
 // Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
 val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
-  from("$projectDir/src/test/resources")
-  into("$buildDir/classes/java/test")
+    from("$projectDir/src/test/resources")
+    into("$buildDir/classes/java/test")
 }
 tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
